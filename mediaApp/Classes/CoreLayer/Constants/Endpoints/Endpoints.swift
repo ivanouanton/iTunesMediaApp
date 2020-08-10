642e1640d3ca8711e62642be6a6a939fabcb6447
Anton Ivanov <anivanov@scand.com>
2020-08-10 14:47:25 +0300

    Install pods. Setup proj hierarchy. Create base classes.

---
+++ mediaApp/Classes/CoreLayer/Constants/Endpoints/Endpoints.swift
@@ -0,0 +1,9 @@
+//
+//  Endpoints.swift
+//  mediaApp
+//
+//  Created by Anton Ivanov on 8/10/20.
+//  Copyright © 2020 Anton Ivanov. All rights reserved.
+//
+
+import Foundation
---
+++ mediaApp/Classes/CoreLayer/CoreComponents/RequestConfigurators/ITunesRequestConfigurator.swift
@@ -0,0 +1,9 @@
+//
+//  ITunesRequestConfigurator.swift
+//  mediaApp
+//
+//  Created by Anton Ivanov on 8/10/20.
+//  Copyright © 2020 Anton Ivanov. All rights reserved.
+//
+
+import Foundation
